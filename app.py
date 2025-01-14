5907582cb511c23f4fcd0afadca9070ca75c26d9
xolotlwastaken <xolotlwastaken@gmail.com>
2025-01-14 14:46:57 +0800

    updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'weoih fowiehf oewihf oewifh ewoifh oiewhf oiewhf oiwehfioewh f'
+    return 'hhelfiehlefh ioeh eiofhei fhief iehfiefh f'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
