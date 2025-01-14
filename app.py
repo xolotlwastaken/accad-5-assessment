d81029a9b504defe79a51b483ff6ad5b06379fd1
xolotlwastaken <xolotlwastaken@gmail.com>
2025-01-14 14:14:46 +0800

    modified some files

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is a containerised app. Updating the app in-class.'
+    return 'Hello, World! This is a containerised app. Updating the app in-class. Testing testing testing.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
